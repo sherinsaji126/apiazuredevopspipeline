7d570a17e47a7d87e7f24db17123b9d8c768edc5
sherinsaji126 <sherinsaji126@gmail.com>
2025-05-26 11:52:40 +0530

    my initial commit

--- apiazuredevopspipeline/Program.cs
+++ apiazuredevopspipeline/Program.cs
@@ -19,9 +19,11 @@ namespace apiazuredevopspipeline
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+              
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
